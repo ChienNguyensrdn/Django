83d21da765626c7f72ca68b4d2242519a5b347e2
Chiến Nguyễn <33652672+ChienNguyensrdn@users.noreply.github.com>
2025-01-06 10:39:34 +0700

    no message

--- src/ELearning/ELearning/settings.py
+++ src/ELearning/ELearning/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ELearning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoDB',
+        'USER': 'myuser',
+        'PASSWORD': 'Aa@123456',
+        'HOST': 'db',  # Tên dịch vụ trong docker-compose
+        'PORT': 5432,
     }
 }
 
